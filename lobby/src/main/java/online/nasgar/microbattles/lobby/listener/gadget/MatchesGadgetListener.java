2466e10320803484ebea766d3e87abcfaff4c953
Aitooor <aitorarias19@gmail.com>
2022-08-26 04:00:18 +0200

    feat: added all the code removed by the rat ex dev

---
+++ lobby/src/main/java/online/nasgar/microbattles/lobby/listener/gadget/MatchesGadgetListener.java
@@ -0,0 +1,43 @@
+package online.nasgar.microbattles.lobby.listener.gadget;
+
+import com.cryptomorin.xseries.XSound;
+import online.nasgar.microbattles.api.adapt.NbtHandler;
+import online.nasgar.microbattles.api.sound.SoundPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import javax.inject.Inject;
+
+public class MatchesGadgetListener implements Listener {
+  private final NbtHandler nbtHandler;
+
+  @Inject
+  public MatchesGadgetListener(NbtHandler nbtHandler) {
+    this.nbtHandler = nbtHandler;
+  }
+
+  @EventHandler
+  public void onPlayerInteract(PlayerInteractEvent event) {
+    Player player = event.getPlayer();
+    ItemStack itemStack = event.getItem();
+
+    if (itemStack == null) {
+      return;
+    }
+
+    String clickable = nbtHandler.getTag(
+      itemStack,
+      "microbattles:clickable"
+    );
+
+    if (clickable == null || !clickable.equals("matches")) {
+      return;
+    }
+
+    SoundPlayer.playNormal(player, XSound.UI_BUTTON_CLICK);
+    player.performCommand("match list");
+  }
+}
